cdceff5d481520703d97e3eb05edbe0f984cf35b
Angel Santana <91813200+iamAngelSH@users.noreply.github.com>
2022-10-04 11:19:11 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,3 +28,8 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# CONVERT JSON DATA INTO A PANDAS DATAFRAME
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# DISPLAY THE TABLE ON THE PAGE
+streamlit.dataframe(fruityvice_normalized)
