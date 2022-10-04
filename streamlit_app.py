09b277df03f3572509f878dd7b19f1ba4edd6cc1
Angel Santana <91813200+iamAngelSH@users.noreply.github.com>
2022-10-04 11:16:08 -0600

    Update streamlit_app.py
    Added requests package : Fruityvice api integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
 import pandas as pd
+import requests
+
 
 streamlit.title('My Parents New Healthy Dinner')
 
@@ -21,3 +23,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # DISPLAY THE TABLE ON THE PAGE
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header('Fruityvice Fruit Advice!')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
